564a47dc609daa975193cbcd196daddfff7edb4c
Lukáš Chylík <lukas.chylik@riganti.cz>
2023-04-16 17:06:34 +0200

    nav menu

--- scripts/init.ts
+++ scripts/init.ts
@@ -41,7 +41,9 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.getElementById('menuBtn') as HTMLButtonElement
+    const menu = document.querySelector('nav ul') as HTMLButtonElement
     menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('burger--open')
+        menu.classList.toggle('open')
     })
 })
