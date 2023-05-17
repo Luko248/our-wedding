7c1c694af6d7fb1ceda62ed03221456dbd7f845a
Lukáš Chylík <lukas.chylik@riganti.cz>
2023-05-17 20:25:08 +0200

    map window styled

--- scripts/init.ts
+++ scripts/init.ts
@@ -10,22 +10,18 @@ import { initNav } from "./_nav";
 // }
 
 function initMap(): void {
-    let mapElement = document.getElementById('mapElem') as HTMLDivElement
+    let mapElement = document.getElementById('mapElem') as HTMLDivElement;
     var centerPosition;
-    const mobileCenterPosition = { lat: 49.415000, lng: 18.570000 }
-    const laptopCenterPosition = { lat: 49.412290, lng: 18.572500 }
-    const desktopCenterPosition = { lat: 49.412290, lng: 18.574300 }
+    const mobileCenterPosition = { lat: 49.415000, lng: 18.570000 };
+    const laptopCenterPosition = { lat: 49.412290, lng: 18.572500 };
+    const desktopCenterPosition = { lat: 49.412290, lng: 18.574300 };
 
     if (window.innerWidth < 768) {
-        centerPosition = mobileCenterPosition
-    }
-
-    else if (window.innerWidth >= 768 && window.innerWidth < 1200) {
-        centerPosition = laptopCenterPosition
-    }
-
-    else{
-        centerPosition = desktopCenterPosition
+        centerPosition = mobileCenterPosition;
+    } else if (window.innerWidth >= 768 && window.innerWidth < 1200) {
+        centerPosition = laptopCenterPosition;
+    } else {
+        centerPosition = desktopCenterPosition;
     }
 
     const map = new google.maps.Map(
@@ -58,9 +54,14 @@ function initMap(): void {
     ];
 
     const infoWindow = new google.maps.InfoWindow({
-        content: '<div class="map__widnow"><strong>Penzión Kriváň<strong><br><span>Lucie & Lukáš Svatba</span></div>',
+        content: '<div class="map__widnow"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt8gNB43uu0kQeLv784HwPOFhVMiAPwJkNmAGAQsgqfIADJS8yg0OVrJiSZCo0LQqhhR0&usqp=CAU" alt="Penzión Kriváň" loading="lazy"></img><address><strong>Penzión Kriváň</strong><br>Dubáčovci 509, 023 21 Korňa<br>Slovensko</address></div>',
         ariaLabel: "Penzión Kriváň",
     });
+
+    // Function to close infoWindow
+    const closeInfoWindow = () => {
+        infoWindow.close();
+    };
 
     for (let i = 0; i < positions.length; i++) {
         const marker = new google.maps.Marker({
@@ -76,9 +77,19 @@ function initMap(): void {
             });
         });
     }
+
+    window.addEventListener("click", (event) => {
+        if (!mapElement.contains(event.target as Node)) {
+            closeInfoWindow();
+        }
+    });
+
+    map.addListener("click", () => {
+        closeInfoWindow();
+    });
 }
 
-initMap()
+initMap();
 
 document.addEventListener('DOMContentLoaded', () => {
     initNav()
