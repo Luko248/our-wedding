ecd1dca1f558150fa722dc64f6e88f632e35a9bc
Lukáš Chylík <lukas.chylik@riganti.cz>
2023-05-17 18:42:53 +0200

    sticky nav styled

--- scripts/_nav.ts
+++ scripts/_nav.ts
@@ -1,4 +1,17 @@
 export const initNav = () => {
+    const body = document.body as HTMLBodyElement
+    const nav = document.querySelector('nav') as HTMLButtonElement
+    const scrollWatcher = document.createElement('div')
+
+    scrollWatcher.setAttribute('data-scroll-watcher', '')
+    nav.before(scrollWatcher)
+
+    const navObserver = new IntersectionObserver((entries) => {
+        nav.classList.toggle('sticked', !entries[0].isIntersecting)
+    }, {rootMargin: '200px 0px 0px 0px'})
+
+    navObserver.observe(scrollWatcher)
+
     const menuBtn = document.getElementById('menuBtn') as HTMLButtonElement
     const menu = document.querySelector('nav ul') as HTMLButtonElement
     
